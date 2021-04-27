a1500b4925c85f91be89c3a25825119fef796440
Lukasz Kozuchowski <lukasz.kozuchowski@gmail.com>
2021-04-27 17:28:46 +0200

    Remove SSL enforcement

--- example_deployment_project/settings.py
+++ example_deployment_project/settings.py
@@ -116,9 +116,9 @@ USE_L10N = True
 USE_TZ = True
 
 
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
-SECURE_SSL_REDIRECT = True
+#CSRF_COOKIE_SECURE = True
+#SESSION_COOKIE_SECURE = True
+#SECURE_SSL_REDIRECT = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
